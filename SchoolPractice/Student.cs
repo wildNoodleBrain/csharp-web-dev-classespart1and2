3d4b53925159cbcc40155e391d9d9154cf782fdb
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-08-15 18:59:09 -0500

    Question 1 Done?

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -5,11 +5,26 @@
 
 namespace SchoolPractice
 {
-    public class Student
+    public class Student 
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        private static int nextStudentId = 1;
+        private string Name { get; set; }
+        private int StudentId { get; set; }
+        private int NumberOfCredits { get; set; } = 0;
+        private double Gpa { get; set; } = 0.0;
+
+        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+        public Student(string name, int studentId): this(name, studentId, 0, 0) { }
+
+        public Student(string name): this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
     }
 }

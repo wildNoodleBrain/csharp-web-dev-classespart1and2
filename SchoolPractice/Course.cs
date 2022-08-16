23df4e5f0dbd013016c15162c75e08e30f81edcd
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-08-15 19:21:15 -0500

    Question 3 done?

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        private string subject;
+        private Teacher instructor;
+        private List<Course> courses;
+    }
+}

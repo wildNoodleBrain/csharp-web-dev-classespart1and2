436d848325ffcac39fb3c06e7f26e6b798cfdf1a
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-08-15 19:20:03 -0500

    Question 2 Done

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,8 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student emily = new Student("Emily", 1, 1, 4.0);
+            Console.WriteLine("The Student class works! " + emily.Name + " is a student!");
         }
     }
 }
